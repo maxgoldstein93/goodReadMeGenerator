941364b034bbf16f11d0d0808f026582ecd45653
Max Goldstein <magoldstein93@gmail.com>
2020-09-24 15:54:43 -0400

    working badges and table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,38 +1,46 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.Title}
+return `# ${data.Title}
   
-  ## License
+## License
+  
+![License: ${data.License}](https://img.shields.io/badge/License-${data.License}-yellow.svg)
 
-  ![License](https://img.shields.io/static/v1?label=${data.License}&message=license&color=red)
+## Description
 
-  ## Description
+${data.Description}
 
-  ${data.Description}
+## Table of Contents
 
-  ## Table of Contents
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+* [License](#License)
 
-  ## Installation
+## Installation
 
-  To install necessary dependencies, run the following command:
+To install necessary dependencies, run the following command:
 
-  ${data.Dependencies}
+${data.Dependencies}
 
-  ## Usage
+## Usage
 
   ${data.Usage}
 
-  ## Contributing
+## Contributing
 
-  ${data.Contribution}
+${data.Contribution}
 
-  # Tests
+# Tests
   To run tests, run the following command:
   
-  ${data.Tests}
+${data.Tests}
 
-  # Questions
-  If you have any questions about the repo, open an issue or contact me directly at ${data.Email}. You can find more of my work at ${data.Github}.
+# Questions
+If you have any questions about the repo, open an issue or contact me directly at ${data.Email}. You can find more of my work on my GitHub ${data.Github}.
 
 `;
 }
